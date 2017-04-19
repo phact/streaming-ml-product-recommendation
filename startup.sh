e3cc14ef343e56c4c519073304993098c18f3ff0
phact <estevezsebastian@gmail.com>
2017-04-19 10:33:28 -0400

    fix order of startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,7 @@ if [ -f /etc/lsb-release ]; then
   apt-get install maven -y
 fi
 
+echo 'Checking for dsefs'
 #check for dsefs
 PORT_CHECK=$(lsof -i:5598|grep LISTEN|wc -l)
 if [ "$PORT_CHECK" -eq 0 ]; then
@@ -16,23 +17,11 @@ if [ "$PORT_CHECK" -eq 0 ]; then
   exit 1;
 fi
 
+echo 'Pushing file to dsefs'
 #push file to dsefs
 dse fs "put ./sales_observations sales_observations"
 
-#build and run spark job
-mvn package && dse spark-submit --deploy-mode cluster --supervise  --class com.datastax.powertools.analytics.SparkMLProductRecommendationServeJDBC ./target/StreamingMLProductRecommendations-0.1.jar localhost 9999 &> nohup.out &
-
-echo "now other stuff"
-
-i=1
-#kick off nc sockets and add data
-while [ true ];
-do
-    echo "while $i";
-    while read -r line; sleep 1; do echo $line | awk -F'\t' -v OFS='\t' '{print $0 $1 $2+$i}'; done < ./sales_observations | nc -lk 9999
-    i=$((i+1));
-done
-
+echo "standing up docs"
 #standup the docs
 #install hugo
 mkdir /opt
@@ -43,3 +32,17 @@ tar -xvf hugo_0.20.2_Linux-64bit.tar.gz -C /opt/hugo
 git submodule update --init
 git submodule sync
 /opt/hugo/hugo_0.20.2_linux_amd64/hugo_0.20.2_linux_amd64 server ./content&
+
+echo "running streaming job"
+#build and run spark job
+mvn package && nohup dse spark-submit --deploy-mode cluster --supervise  --class com.datastax.powertools.analytics.SparkMLProductRecommendationServeJDBC ./target/StreamingMLProductRecommendations-0.1.jar localhost 9999&
+
+echo "generating stream"
+i=1
+#kick off nc sockets and add data
+while [ true ];
+do
+    echo "while $i";
+    while read -r line; sleep 1; do echo $line | awk -F'\t' -v OFS='\t' '{print $0 $1 $2+$i}'; done < ./sales_observations | nc -lk 9999
+    i=$((i+1));
+done
